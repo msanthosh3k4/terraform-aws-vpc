bea03b03aceccf4bf203202fb79959334968574e
msanthosh3k4 <m.santhosh3k4@gmail.com>
2025-10-28 13:05:16 +0530

    terraform vpc module

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable name {
+
+}
+
+variable "cidr_block" {
+
+}
+
+variable "enable_dns_hostnames" {
+  default     = true
+}
+
+variable "common_tags" {
+    type = map
+}
+
+variable "environment"{
+
+}
+
+variable "project_name" {
+    
+}
