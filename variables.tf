8edd123d6477f3b65ec3f61de0b7a9e220504d05
msanthosh3k4 <m.santhosh3k4@gmail.com>
2025-10-29 16:12:45 +0530

    vpc-module

--- variables.tf
+++ variables.tf
@@ -1,7 +1,3 @@
-variable name {
-
-}
-
 variable "cidr_block" {
 
 }
@@ -10,14 +6,45 @@ variable "enable_dns_hostnames" {
   default     = true
 }
 
-variable "common_tags" {
-    type = map
-}
+# variable "common_tags" {
 
-variable "environment"{
+# }
 
-}
+# variable "environment"{
 
-variable "project_name" {
+# }
+
+# variable "project_name" {
     
+# }
+variable "resource_name" {
+
+}
+
+variable "public_subnet_cidrs" {
+  type = list
+    validation {
+    condition     = length(var.public_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid public subnet CIDR"
+  }
+}
+
+variable "private_subnet_cidrs" {
+    type = list
+   validation {
+    condition     = length(var.private_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid private subnet CIDR"
+  }
+}
+
+variable "database_subnet_cidrs" {
+    type = list
+   validation {
+    condition     = length(var.database_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid private subnet CIDR"
+  }
+}
+
+variable "ispeering_required" {
+  default = false
 }
