ac5611afa9f3cf09d7c96e646253d77fd472f968
msanthosh3k4 <m.santhosh3k4@gmail.com>
2025-10-29 16:11:20 +0530

    vpc-Module

--- locals.tf
+++ locals.tf
@@ -1,3 +1,14 @@
+# locals {
+#   resource_name = "${var.project_name}-${var.environment}"
+#   vpc_tags = {
+#     terraform = true
+#   }
+# }
 locals {
-  resource_name = "${var.project_name}-${var.environment}"
+  vpc_tags = {
+    terraform = true
+  }
+  az_available = slice(data.aws_availability_zones.available.names, 0, 2)
+  default_vpc = data.aws_vpc.default_vpc.id
+  default_cidr = data.aws_vpc.default_vpc.cidr_block
 }
