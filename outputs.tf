e9221118bee7b4d3f5d63a70445f04ea6193f1ff
msanthosh3k4 <m.santhosh3k4@gmail.com>
2025-11-06 21:28:51 +0530

    vpc-module

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,18 @@
 # output "default_vpc" {
 #       value       = data.aws_vpc.default_vpc
 # }
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
